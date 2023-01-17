fdc9ecb58cd318b286cada0ee9678211db24a688
liav <liavtoledano@gmail.com>
2023-01-18 00:53:36 +0200

    changed ofer's bytes2int func

--- byte_func.py
+++ byte_func.py
@@ -1,30 +1,38 @@
+import codecs
+
 def bytes2int(str):
- return int(str.encode('hex'), 16)
+    hexlify = codecs.getencoder('hex')
+    return int(hexlify(str)[0], 16)
+
 
 def bytes2hex(str):
- return '0x'+str.encode('hex')
+    return '0x' + str.encode('hex')
+
 
 def int2bytes(i):
- h = int2hex(i)
- return hex2bytes(h)
+    h = int2hex(i)
+    return hex2bytes(h)
+
 
 def int2hex(i):
- return hex(i)
+    return hex(i)
+
 
 def hex2int(h):
- if len(h) > 1 and h[0:2] == '0x':
-  h = h[2:]
+    if len(h) > 1 and h[0:2] == '0x':
+        h = h[2:]
 
- if len(h) % 2:
-  h = "0" + h
+    if len(h) % 2:
+        h = "0" + h
 
- return int(h, 16)
+    return int(h, 16)
+
 
 def hex2bytes(h):
- if len(h) > 1 and h[0:2] == '0x':
-  h = h[2:]
+    if len(h) > 1 and h[0:2] == '0x':
+        h = h[2:]
 
- if len(h) % 2:
-  h = "0" + h
+    if len(h) % 2:
+        h = "0" + h
 
- return h.decode('hex')
+    return h.decode('hex')
