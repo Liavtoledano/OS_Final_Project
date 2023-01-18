a9d8784f5211f97d5129d8a4f547fd93afd1ee26
liav <liavtoledano@gmail.com>
2023-01-18 13:50:34 +0200

    working for all frames

--- Start.py
+++ Start.py
@@ -1,46 +1,44 @@
-import byte_func
 import numpy as np
+import matplotlib.pyplot as plt
 
+# Define the function to find the matrices
+def find_matrices(frame):
+    # Calculate the average value of the bytes
+    avg = np.mean(frame)
+    mul_avg = 1.3 * avg
+    # Calculate the lower and upper bounds of the range
+    lower_bound = mul_avg - (mul_avg * 0.05)
+    upper_bound = mul_avg + (mul_avg * 0.05)
+    # Initialize a variable to store the 3x3 matrices
+    matrices = []
+    idx = []
+    # Iterate over each index in the data
+    for i in range(frame.shape[0]-2):
+        for j in range(frame.shape[1]-2):
+            matrix = frame[i:i+3, j:j+3]
+            if np.all(lower_bound <= matrix) and np.all(matrix <= upper_bound):
+                matrices.append(matrix)
+                idx.append((j+1,i+1))
+    return matrices, idx
 
-
-if __name__ == "__main__":
-    # file = open("data\\data\\fr0.bin", "rb")
-    # image_size = 600*800
-    # byte = file.read(1)
-    # cnt = 0
-    # summ = 0
-    # for line in file:
-    #     for byte in line:
-    #         summ += byte
-    #
-    # avg = summ/image_size
-    # print(avg)
-    # file.close()
-
-    with open('data\\data\\fr0.bin', 'rb') as file:
-        # Read the contents of the file into a variable
-        data = file.read()
-        # Convert the data to a numpy array
-        data = np.frombuffer(data, dtype=np.uint8)
-        # reshape the data to 600x800
-        data = data.reshape(600, 800)
-        # Calculate the average value of the bytes
-        avg = np.mean(data)
-        mul_avg = 1.3 * avg
-        # Calculate the lower and upper bounds of the range
-        lower_bound = mul_avg - (mul_avg * 0.05)
-        upper_bound = mul_avg + (mul_avg * 0.05)
-        # Initialize a variable to store the 3x3 matrices
-        matrices = []
-        indexes = []
-        # Iterate over each index in the data
-        for i in range(data.shape[0] - 2):
-            for j in range(data.shape[1] - 2):
-                matrix = data[i:i + 3, j:j + 3]
-                if np.all(lower_bound <= matrix) and np.all(matrix <= upper_bound):
-                    matrices.append(matrix)
-                    # save the middle indexes of the matrix
-                    indexes.append((i + 1, j + 1))
-        # Print the 3x3 matrices that are within the range
-        print("3x3 matrices within the range:", matrices)
-        print(indexes)
+# Open the video file in read mode
+with open('data\\data\\fr1.bin', 'rb') as file:
+    # Read the contents of the file into a variable
+    data = file.read()
+    # Get the number of frames
+    num_of_frames = len(data) // (600*800)
+    # reshape the data to 600x800xnum_of_frames
+    data = np.frombuffer(data, dtype=np.uint8).reshape(num_of_frames, 600, 800)
+    # Initialize a variable to store the 3x3 matrices for all frames
+    all_matrices = []
+    all_idx = []
+    for i in range(num_of_frames):
+        frame = data[i,:,:]
+        result = find_matrices(frame)
+        all_matrices.append(result[0])
+        all_idx.append(result[1])
+        plt.imshow(frame, cmap='gray')
+        plt.scatter(result[1][0][0], result[1][0][1], color="red")
+        plt.show()
+    # Print the 3x3 matrices that are within the range for all frames
+    print("3x3 matrices within the range for all frames:", all_matrices)
